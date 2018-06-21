b81a2e78e1440104a2039c5a21b172c68bebeef0
maxim.narushevich <maxi@digitalmind.be>
2018-06-21 17:14:16 +0300

    - Removed unnecessary connecting of jQuery library for Pagebuilder page

--- Modules/Pagebuilder/Resources/views/index.blade.php
+++ Modules/Pagebuilder/Resources/views/index.blade.php
@@ -12,10 +12,6 @@
 @stop
 @section('General')
     <div id="pagebuilder">
-        <script
-                src="https://code.jquery.com/jquery-1.11.1.min.js"
-                integrity="sha256-VAvG3sHdS5LqTT+5A/aeq/bZGa/Uj04xKxY8KM/w9EE="
-                crossorigin="anonymous"></script>
     <h3>Pagebuilder</h3>
 
 
