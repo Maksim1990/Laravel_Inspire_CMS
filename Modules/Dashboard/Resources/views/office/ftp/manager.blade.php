dc9ee5e07e0c05d97f59c6246351e07916584a41
maxim.narushevich <maxi@digitalmind.be>
2018-08-14 16:25:22 +0300

    - Improved FTP manager layout

--- Modules/Dashboard/Resources/views/office/ftp/manager.blade.php
+++ Modules/Dashboard/Resources/views/office/ftp/manager.blade.php
@@ -12,7 +12,7 @@
                 </div>
 
 
-                <div class="col-sm-12 col-md-3  col-xs-12 text-center icon_module">
+                <div class="col-sm-12 col-md-3 col-md-offset-1  col-xs-12 text-center icon_module">
                     <a href="{{route('office_ftp_content',['id'=>Auth::id()])}}">
                         <p>
                             <img  height="100"
@@ -21,15 +21,16 @@
                         <span>@lang('dashboard::messages.ftp_see_content')</span>
                     </a>
                 </div>
-                <div class="col-sm-12 col-md-3  col-xs-12 text-center icon_module">
+                <div class="col-sm-12 col-md-3 col-md-offset-1   col-xs-12 text-center icon_module">
                     <a href="{{route('office_ftp_connection',['id'=>Auth::id()])}}">
                         {{--<p><i class="fa fa-spinner w3-spin" style="font-size:64px"></i></p>--}}
                         <p class="font_icon"><i class="fas fa-wrench"></i></p>
                         @lang('dashboard::messages.ftp_update_credentials')
                     </a>
                 </div>
+                
                 @if(Auth::user()->admin==1)
-                    <div class="col-sm-12 col-md-3  col-xs-12 text-center icon_module">
+                    <div class="col-sm-12 col-md-3 col-md-offset-1 col-xs-12 text-center icon_module">
                         <a href="{{route('office_ftp_connection_admin',['id'=>Auth::id()])}}">
                             <p class="font_icon"><i class="fas fa-cogs"></i></p>
                             @lang('dashboard::messages.ftp_update_credentials_admin')
