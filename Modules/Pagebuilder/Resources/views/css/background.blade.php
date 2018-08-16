c15b8d3f485c12b06ede221df6ea52f2387d91c9
Your Name <you@example.com>
2018-08-16 23:37:19 +0300

    - Added ability to use colpick-master package for color pick on background page

--- Modules/Pagebuilder/Resources/views/css/background.blade.php
+++ Modules/Pagebuilder/Resources/views/css/background.blade.php
@@ -1,7 +1,8 @@
 @extends('pagebuilder::layouts.master')
 
 @section('styles')
-
+    <link rel="stylesheet" href="{{custom_asset('plugins/vendor/colpick-master/css/colpick.css')}}">
+    <script src="{{custom_asset('plugins/vendor/colpick-master/js/colpick.js')}}"></script>
 @stop
 @section('scripts_header')
 
@@ -15,7 +16,95 @@
                     <h3 class="title">Background</h3>
                     <div id="title_shape"></div>
                 </div>
+                <h3>Button</h3>
+                <p>
+                    <button id="example-button" value="#00FF00">Show Color Picker</button>
+                </p>
+                <p>
+                    <code>$('#example-button').colpick();</code>
+                </p>
+                <script>
+                    $('#example-button').colpick();
+                </script>
 
+
+                <h3>Flat mode and hex layout</h3>
+                <p>
+                <div id="example-flat"></div>
+                </p>
+                <p>
+                    <code>$('#example-flat').colpick({
+                        color:'123456',
+                        flat:true,
+                        layout:'hex'
+                        });</code>
+                </p>
+                <script>
+                    $('#example-flat').colpick({
+                        color:'123456',
+                        flat:true,
+                        layout:'hex'
+                    });
+                </script>
+
+
+                <h3>Text Input and colorScheme 'dark'</h3>
+                <p>
+                    <input type="text" id="example-text" value="#0000FF">
+                </p>
+                <p>
+                    <code>$('#example-text').colpick({
+                        colorScheme:'dark',
+                        onChange:function(hsb,hex,rgb,el,bySetColor) {
+                        $(el).val('#'+hex);
+                        }
+                        });</code>
+                </p>
+                <script>
+                    $('#example-text').colpick({
+                        colorScheme:'dark',
+                        onChange:function(hsb,hex,rgb,el,bySetColor) {
+                            $(el).val('#'+hex);
+                        }
+                    });
+                </script>
+
+                <h3>HTML5 Color Input</h3>
+                <p>
+                    <input type="color" id="example-color" value="#FF0000">
+                </p>
+                <p>
+                    <code>$('#example-color').colpick({
+                        onSubmit:function(hsb,hex,rgb,el,bySetColor) {
+                        $(el).val('#'+hex);
+                        $(el).colpickHide();
+                        }
+                        });</code>
+                </p>
+                <script>
+                    $('#example-color').colpick({
+                        onSubmit:function(hsb,hex,rgb,el,bySetColor) {
+                            $(el).val('#'+hex);
+                            $(el).colpickHide();
+                        }
+                    });
+                </script>
+
+
+                <h3>Polyfill</h3>
+                <p>
+                    <input type="color" id="example-polyfill">
+                </p>
+                <p>
+                    <code>$('#example-polyfill').colpick({
+                        polyfill:true
+                        });</code>
+                </p>
+                <script>
+                    $('#example-polyfill').colpick({
+                        polyfill:true
+                    });
+                </script>
 
             </div>
         </div>
