228607152b33de52d0f717b95ed3c3685ee2aa0d
Your Name <you@example.com>
2018-08-18 23:26:15 +0300

    - Fixed issue with undefined background type in Website module

--- Modules/Website/Resources/views/blocks/block5.blade.php
+++ Modules/Website/Resources/views/blocks/block5.blade.php
@@ -1,9 +1,9 @@
 <!-- Container (Portfolio Section) -->
 @php
 
-    if($background->background_type=='color'){
+    if(!empty($background) && $background->background_type=='color'){
     $strBackground="background-color:".$background->color.";";
-    }elseif($background->background_type=='image'){
+    }elseif(!empty($background) && $background->background_type=='image'){
     $strBackground="background: url('".custom_asset("storage/".$background->image_path)."') no-repeat center center fixed;
         min-height: 100%;
         background-size: cover;
