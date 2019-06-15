0eb226744f18c5d7bf88695b91ef4b38362444b0
Maksim <narushevich.maksim@gmail.com>
2019-06-15 19:03:22 +0300

    Update mapping_docker_env_without_mysql.sh

--- deploy/mapping_docker_env_without_mysql.sh
+++ deploy/mapping_docker_env_without_mysql.sh
@@ -4,6 +4,8 @@ sed -e "s/\${APP_HTTP_PORT}/${DEV_HTTP_PORT}/g;
              s/\${APP_HTTPS_PORT}/${DEV_HTTPS_PORT}/g;
              s/\${APP_REDIS_PORT}/${DEV_REDIS_PORT}/g;"  ./deploy/docker-compose.dev.tpl.yml > ./docker-compose.yml
 
+cat ./docker-compose.yml
+
 sed -e "s/\${APP_MYSQL_PASSWORD}/${DEV_SHARED_MYSQL_PASSWORD}/g;
  s/\${APP_MYSQL_DATABASE}/${DEV_MYSQL_DATABASE}/g;
  s/\${APP_MAILGUN_DOMAIN}/${DEV_MAILGUN_DOMAIN}/g;
